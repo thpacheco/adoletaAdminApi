37c59fb801663d0d84c5ea506ad7514cfd102eab
Thiago Xavier Pacheco <thiago.pacheco@ltm.digital.com>
2021-03-07 22:13:03 -0300

    Incluindo novas rotas

--- controllers/users/usersController.go
+++ controllers/users/usersController.go
@@ -11,8 +11,10 @@ import (
 
 	userModel "adoletaAdminApi/models/users"
 
+	"github.com/gorilla/mux"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var userCollection = cosmoDb.Db().Database("goTest").Collection("users")
@@ -59,4 +61,68 @@ func GetAllUsers(w http.ResponseWriter, r *http.Request) {
 	}
 	cur.Close(context.TODO()) // close the cursor once stream of documents has exhausted
 	json.NewEncoder(w).Encode(results)
+}
+
+func GetUserProfile(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	var body userModel.User
+
+	e := json.NewDecoder(r.Body).Decode(&body)
+	if e != nil {
+		fmt.Print(e)
+	}
+	var result primitive.M //  an unordered representation of a BSON //document which is a Map
+
+	err := userCollection.FindOne(context.TODO(), bson.D{{"name", body.Name}}).Decode(&result)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	json.NewEncoder(w).Encode(result) // returns a Map containing //mongodb document
+}
+
+func UpdateProfile(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-Type", "application/json")
+
+	var body userModel.UpdateBody
+
+	e := json.NewDecoder(r.Body).Decode(&body)
+	if e != nil {
+
+		fmt.Print(e)
+	}
+
+	filter := bson.D{{"name", body.Name}} // converting value to BSON type
+	after := options.After                // for returning updated document
+
+	returnOpt := options.FindOneAndUpdateOptions{
+
+		ReturnDocument: &after,
+	}
+	update := bson.D{{"$set", bson.D{{"city", body.City}}}}
+	updateResult := userCollection.FindOneAndUpdate(context.TODO(), filter, update, &returnOpt)
+
+	var result primitive.M
+	_ = updateResult.Decode(&result)
+
+	json.NewEncoder(w).Encode(result)
+}
+
+func DeleteProfile(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)["id"]                   //get Parameter value as string
+	_id, err := primitive.ObjectIDFromHex(params) // convert params to //mongodb Hex ID
+	if err != nil {
+		fmt.Printf(err.Error())
+	}
+	opts := options.Delete().SetCollation(&options.Collation{}) // to //specify language-specific rules for string comparison, such as //rules for lettercase
+
+	res, err := userCollection.DeleteOne(context.TODO(), bson.D{{"_id", _id}}, opts)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	json.NewEncoder(w).Encode(res.DeletedCount) // return number of //documents deleted
 }
--- main.go
+++ main.go
@@ -14,14 +14,13 @@ func main() {
 	route := mux.NewRouter()
 
 	s := route.PathPrefix("/api").Subrouter() //Base Path
-	//Routes
 
-	// s.Handle("/createProfile", jwtService.IsAuthorized(userController.CreateProfile)).Methods("POST")
+	//Routes
+	s.Handle("/createProfile", jwtService.IsAuthorized(usersController.CreateProfile)).Methods("POST")
 	s.Handle("/getAllUsers", jwtService.IsAuthorized(usersController.GetAllUsers)).Methods("GET")
-	// s.Handle("/getUserProfile", jwtService.IsAuthorized(userController.GetUserProfile)).Methods("POST")
-	// s.Handle("/updateProfile", jwtService.IsAuthorized(userController.UpdateProfile)).Methods("PUT")
-	// s.Handle("/deleteProfile/{id}", jwtService.IsAuthorized(userController.DeleteProfile)).Methods("DELETE")
-	s.HandleFunc("/createProfile", usersController.CreateProfile).Methods("POST")
+	s.Handle("/getUserProfile", jwtService.IsAuthorized(usersController.GetUserProfile)).Methods("POST")
+	s.Handle("/updateProfile", jwtService.IsAuthorized(usersController.UpdateProfile)).Methods("PUT")
+	s.Handle("/deleteProfile/{id}", jwtService.IsAuthorized(usersController.DeleteProfile)).Methods("DELETE")
 	s.HandleFunc("/auth", authController.Auth).Methods("POST")
 
 	log.Fatal(http.ListenAndServe(":8000", s)) // Run Server
