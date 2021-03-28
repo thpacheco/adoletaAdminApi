593a395c038221c4816eda9a76672ea2f6578809
Thiago Xavier Pacheco <thiago.pacheco@ltm.digital.com>
2021-03-28 20:58:25 -0300

    adicionando conectstring

--- db/cosmoDb.go
+++ db/cosmoDb.go
@@ -10,7 +10,7 @@ import (
 )
 
 func Db() *mongo.Client {
-	var stringUrl string = "Sua ConnectionString"
+	var stringUrl string = "mongodb://antares-padaria:dWThknIXmCHIfydERRpRi26T6eXc6fb2UpBiZUnWCmHvCxJvGjNfWwCCg3eiLkmpB2OKLKw6iZNefsijQPqnyg==@antares-padaria.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@antares-padaria@"
 	clientOptions := options.Client().ApplyURI(stringUrl) // Connect to //MongoDB
 	client, err := mongo.Connect(context.TODO(), clientOptions)
 	if err != nil {
