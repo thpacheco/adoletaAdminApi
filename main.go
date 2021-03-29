8790bd8a247cbd775a48b17ca9b66cc6eefaf7ad
Thiago Xavier Pacheco <thiago.pacheco@ltm.digital.com>
2021-03-28 22:48:48 -0300

    retirando connectString

--- main.go
+++ main.go
@@ -4,10 +4,8 @@ import (
 	authController "adoletaAdminApi/controllers/auth"
 	usersController "adoletaAdminApi/controllers/users"
 	jwtService "adoletaAdminApi/jwtSecurity"
-	"fmt"
 	"log"
 	"net/http"
-	"os"
 
 	"github.com/gorilla/mux"
 	"github.com/rs/cors"
@@ -19,12 +17,15 @@ func main() {
 	s := route.PathPrefix("/api").Subrouter() //Base Path
 
 	//Routes
-	s.Handle("/createProfile", jwtService.IsAuthorized(usersController.CreateProfile)).Methods("POST")
+	// s.Handle("/createProfile", jwtService.IsAuthorized(usersController.CreateProfile)).Methods("POST")
 	s.Handle("/getAllUsers", jwtService.IsAuthorized(usersController.GetAllUsers)).Methods("GET")
 	s.Handle("/getUserProfile", jwtService.IsAuthorized(usersController.GetUserProfile)).Methods("POST")
 	s.Handle("/updateProfile", jwtService.IsAuthorized(usersController.UpdateProfile)).Methods("PUT")
 	s.Handle("/deleteProfile/{id}", jwtService.IsAuthorized(usersController.DeleteProfile)).Methods("DELETE")
 	s.HandleFunc("/auth", authController.Auth).Methods("POST")
+
+	s.HandleFunc("/getAllUsers", usersController.GetAllUsers).Methods("GET")
+	s.HandleFunc("/createProfile", usersController.CreateProfile).Methods("POST")
 
 	c := cors.New(cors.Options{
 		AllowedOrigins: []string{"*"},
@@ -34,16 +35,6 @@ func main() {
 
 	handler := c.Handler(route)
 
-	log.Fatal(http.ListenAndServe(GetPort(), handler)) // Run Server
+	log.Fatal(http.ListenAndServe(":8000", handler)) // Run Server
 
-}
-
-func GetPort() string {
-	var port = os.Getenv("PORT")
-	// Set a default port if there is nothing in the environment
-	if port == "" {
-		port = "8000"
-		fmt.Println("INFO: No PORT environment variable detected, defaulting to " + port)
-	}
-	return ":" + port
 }
