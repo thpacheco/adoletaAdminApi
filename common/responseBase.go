f1e5f85bc27f5c54ec05fe80f47ffe7d861082ea
Thiago Xavier Pacheco <thiago.pacheco@ltm.digital.com>
2021-04-03 16:45:07 -0300

    commit antes de fazer update

--- common/responseBase.go
+++
@@ -1,9 +0,0 @@
-package responseBase
-
-type Data struct {
-	Success bool
-	Loading bool
-	Data    interface{}
-	Message string
-	Status  int
-}
--- models/auth/authModel.go
+++
@@ -1,7 +0,0 @@
-package authModel
-
-type Auth struct {
-	Name     string `json:"name"`
-	UserName string `json:"username"`
-	Password string `json:"password"`
-}
