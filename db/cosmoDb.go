bc53c0e8934eb3369a1ddd7aa1b867656a7d76b0
Thiago Pacheco <thiagopacheco30@gmail.com>
2021-04-03 11:02:21 -0400

    Retirando ConnectionString

--- db/cosmoDb.go
+++ db/cosmoDb.go
@@ -10,7 +10,7 @@ import (
 )
 
 func Db() *mongo.Client {
-	var stringUrl string = "mongodb://antares-padaria:dWThknIXmCHIfydERRpRi26T6eXc6fb2UpBiZUnWCmHvCxJvGjNfWwCCg3eiLkmpB2OKLKw6iZNefsijQPqnyg==@antares-padaria.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@antares-padaria@"
+	var stringUrl string = "Sua ConnectionString"
 	clientOptions := options.Client().ApplyURI(stringUrl) // Connect to //MongoDB
 	client, err := mongo.Connect(context.TODO(), clientOptions)
 	if err != nil {
@@ -23,5 +23,4 @@ func Db() *mongo.Client {
 	}
 	fmt.Println("Connected to MongoDB!")
 	return client
-
 }
--- go.mod
+++ go.mod
@@ -6,5 +6,5 @@ require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/gorilla/mux v1.8.0
 	github.com/rs/cors v1.7.0 // indirect
-	go.mongodb.org/mongo-driver v1.4.6 // indirect
+	go.mongodb.org/mongo-driver v1.4.6
 )
